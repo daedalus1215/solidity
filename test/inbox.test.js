3958f0882f8b3ec860a102c066ba211f2c678b4e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-04-10 21:27:26 -0400

    test - make sure default message is set.

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -5,6 +5,7 @@ const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile.js');
 let accounts;
 let inbox;
+const expected = 'Hi there!'
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -13,7 +14,7 @@ beforeEach(async () => {
     // use one of those accounts to deploy 
     // the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ data: bytecode, arguments: [expected] })
         .send({ from: accounts[0], gas: '1000000' })
 });
 
@@ -23,4 +24,8 @@ describe('Inbox', () => {
         assert.ok(inbox.options.address);
     });
 
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, expected)
+    });
 })
