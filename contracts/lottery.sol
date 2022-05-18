296b1e6a71045b4f71738c4455665df6edd1372e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-05-18 09:51:50 -0400

    Add lottery scaffolding contract

---
+++ contracts/lottery.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+/**
+* Creating a Lottrery contract. where player 1 and player 2 can send ether into a contract. As soon as they do this, they will be considered a player of the game. 
+* The ether they send will be added to a prize pool of sorts.
+* Third party will come in and tell the contract to pick a winner. At this point the contract will look at the two players and send the prize pool to the winner.
+*/
+contract Lottery {
+    address public manager;
+
+    function Lottery() public {
+        manager = msg.sender; // Grab the sender's address
+
+    }
+}
