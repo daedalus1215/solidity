3d0cc7ad7de14b916b122ed18fa2ae3fb1079c38
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-05-20 20:30:10 -0400

    setup test

---
+++ test/Lottery.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+    // get the list of accounts through web3, provided by ganache.
+    accounts = await web3.eth.getAccounts();
+
+    // deploy an instance of our lottery contract
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address);
+    });
+});
