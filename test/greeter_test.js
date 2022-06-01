8a1e2444d3912b17d4426e3f46b7e3acda5efa21
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-01 18:28:53 -0400

    Make the greeting conditional

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -3,9 +3,11 @@ const { writeContracts } = require("truffle");
 const GreeterContract = artifacts.require('../contracts/Greeter');
 
 contract("Greeter", () => {
-    it('has been deployed successfully', async () => {
-        const greeter = await GreeterContract.deployed();
-        assert(greeter, 'contract was no deployed');
+    describe('deploy', () => {
+        it('has been deployed successfully', async () => {
+            const greeter = await GreeterContract.deployed();
+            assert(greeter, 'contract was no deployed');
+        });
     });
 
     describe('greet()', () => {
@@ -14,6 +16,20 @@ contract("Greeter", () => {
             const expected = 'Hello World!';
             const actual = await greeter.greet();
 
+            assert(actual, expected);
+        })
+    });
+});
+
+contract("Greeter: update greeting", () => {
+    describe('setGreeting(string)', () => {
+        it('sets greeting to passed in string', async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = 'Hi there!';
+
+            await greeter.setGreeting(expected);
+            const actual = await greeter.greet();
+            
             assert(actual, expected);
         })
     });
