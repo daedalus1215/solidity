0724dd067e6c3a8707059075bc6c58f0cf0d9c2f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-05-31 23:00:58 -0400

    start over

---
+++ test/greeter_test.js
@@ -0,0 +1,20 @@
+const { writeContracts } = require("truffle");
+
+const GreeterContract = artifacts.require('../contracts/Greeter');
+
+contract("Greeter", () => {
+    it('has been deployed successfully', async () => {
+        const greeter = await GreeterContract.deployed();
+        assert(greeter, 'contract was no deployed');
+    });
+
+    describe('greet()', () => {
+        it("returns 'Hello World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = 'Hello World!';
+            const actual = await greeter.greet();
+
+            assert.equals(actual, expected, "greeted with 'Hello World!'");
+        })
+    });
+});
