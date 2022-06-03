9de7c80007a248225acc93467ebce2ea2112fb00
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-02 21:37:22 -0400

    Use Openzeppelin for the require owner checking.

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,12 +1,11 @@
 pragma solidity >=0.8.14;
 
-contract Greeter {
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
     string private _greeting = "Hello World!";
     address private _owner;
 
-    constructor() public {
-        _owner = msg.sender;
-    }
     function greet() external view returns (string memory) {
         return _greeting;
     }
@@ -15,16 +14,7 @@ contract Greeter {
         _greeting = greeting;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Ownable: caller is not the owner");
-        _;
-    }
-
     function sender() public view returns (address) {
         return msg.sender;
-    }
-
-    function owner() public view returns (address) {
-        return _owner;
     }
 }
