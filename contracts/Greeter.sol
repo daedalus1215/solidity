5b3edd8f3872019370883abdb052d5044a6b6986
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-06-05 09:32:32 -0400

    Bring in prev contract with new unbox tooling

---
+++ contracts/Greeter.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.21 <8.10.0;
+
+
+contract Greeter {
+    string private _greeting = "Hello World!";
+    address private _owner;
+ 
+    constructor() public {
+        _owner = msg.sender;
+    }
+     function greet() external view returns (string memory) {
+         return _greeting;
+     }
+ 
+    function setGreeting(string calldata greeting) external onlyOwner {
+         _greeting = greeting;
+     }
+ 
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function sender() public view returns (address) {
+        return msg.sender;
+    }
+
+     function owner() public view returns (address) {
+         return _owner;
+     }
+ }
